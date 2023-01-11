84475b6116e26b4706b5c52fd8359e7c28b4abf1
Nour-eddine Taleb <ntaleb@1337.ma>
2023-01-11 19:08:24 +0100

    keep logs for debugging

--- start.sh
+++ start.sh
@@ -24,6 +24,12 @@ echo "\e[92m[+] Given Folder: $1"
 echo "[+] Test Type: $2\e[0m\n"
 echo "\e[94m[+] In Case of a failed test, please check ./errors_log file for more information\e[0m\n"
 
+
+bash <<EOF
+shopt -s extglob
+rm -rf errors_log log+([0-9])?(_+([0-9]))_philo?(_bonus)
+EOF
+
 error_log ()
 {
     echo "[$1-$2]: $3" >> ./errors_log
@@ -31,23 +37,22 @@ error_log ()
 
 test_one ()
 {
-    ("$2/$1/$1" 4 310 200 100 > "./log_$1")&
+    ("$2/$1/$1" 4 310 200 100 > "./log1_$1")&
     sleep 5
     pkill $1
-    output=$(grep died -m 1 "./log_$1" | awk '{print $NF}')
+    output=$(grep died -m 1 "./log1_$1" | awk '{print $NF}')
     if [ "$output" = "died" ];then
         echo "\e[92m[+] Test #1 Succeeded !\e[0m"
     else
         echo "\e[91m[+] Test #1 Failed !\e[0m"
         error_log $1 "Test #1" "Given 4 310 200 100 arguments to $1, a philosopher should die !"
     fi
-    rm -rf "./log_$1"
 }
 
 test_two ()
 {
     echo "\e[94m[+] Test #2: Executing your program for 180 second, please wait...\e[0m"
-    ("$2/$1/$1" 4 410 200 200 > /dev/null)&
+    ("$2/$1/$1" 4 410 200 200 > "./log2_$1")&
     i=1
     error=0
     while [ $i -lt 180 ];do
@@ -72,7 +77,7 @@ test_two ()
 test_three ()
 {
     echo "\e[94m[+] Test #3: Executing your program for 180 second, please wait...\e[0m"
-    ("$2/$1/$1" 5 800 200 200 > /dev/null)&
+    ("$2/$1/$1" 5 800 200 200 > "./log3_$1")&
     i=1
     error=0
     while [ $i -lt 180 ];do
@@ -96,13 +101,13 @@ test_three ()
 
 test_four ()
 {
-    ("$2/$1/$1" 4 410 200 200 $3 > "./log_$1")&
+    ("$2/$1/$1" 4 410 200 200 $3 > "./log4_$4_$1")&
     sleep 10
     pgrep $1 > /dev/null
     if [ "$?" -eq 1 ];then
-        lines=$(grep eating "./log_$1" | wc -l)
-        if [ $lines -ge $4 ];then
-            echo "\t\e[92m[+] Test #4-$5 Succeeded\e[0m"
+        lines=$(grep eating "./log4_$4_$1" | wc -l)
+        if [ $lines -ge $(($3 * 4)) ];then
+            echo "\t\e[92m[+] Test #4-$4 Succeeded\e[0m"
         else
             echo "\t\e[91m[+] Test #4-$5 Failed\e[0m"
             error_log $1 "Test #4" "Given 4 410 200 200 $3 arguments to $1, $1 should only be stopped if each philosopher ate at least $3 times !"
@@ -112,7 +117,6 @@ test_four ()
         error_log $1 "Test #4" "Given 4 410 200 200 $3 arguments to $1, $1 should stop !"
         pkill $1
     fi
-    rm -rf "./log_$1"
 }
 
 test_five ()
@@ -122,12 +126,12 @@ test_five ()
     t=0
     error=0
     while [ $i -le 10 ];do
-        ("$2/$1/$1" 2 60 60 60 > "./log_$1")&
+        ("$2/$1/$1" 2 60 60 60 > "./log5_$1")&
         sleep 2
         pgrep $1 > /dev/null
         if [ "$?" -eq 1 ];then
             printf "\r[%d/10]" $i
-            tmp=$(grep died -m 1 "./log_$1" | awk '{print $1}' | sed 's/[^0-9]*//g')
+            tmp=$(grep died -m 1 "./log5_$1" | awk '{print $1}' | sed 's/[^0-9]*//g')
             if [ $i -gt 1 ];then
                 x=$(expr $tmp - $t)
                 x=${x#-}
@@ -156,7 +160,7 @@ test_five ()
 
 test_six ()
 {
-    ("$2/$1/$1" 10 410 200 200 > /dev/null)&
+    ("$2/$1/$1" 10 410 200 200 > "./log6_$1")&
     sleep 2
     forks=$(pgrep $1 | wc -l)
     if [ "$forks" -eq 11 ];then
